04d4dfee7fda6fb1cefa9a3d0ddc9a414e7acc37
drmargarido <drmargarido@gmail.com>
2021-03-07 03:15:01 +0000

    Added initial implementation for the context menu. Made the telescope and the drawer in the room use the context menu as a first test.

---
+++ codenameGOD/main/context_data.lua
@@ -0,0 +1,8 @@
+--[[ Each entry should have the following format
+	[object_name] = {
+		{text="Name", click=function() print("Selected!") end},
+		{text="Name2", click=function() print("Selected2!") end}
+	}
+]]
+
+return {}
