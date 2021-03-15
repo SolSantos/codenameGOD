ca47ca86da68161ae2d1ab328a1bc142575e99ce
Guilherme Silva <32681069+gcsilva11@users.noreply.github.com>
2021-03-15 13:22:41 +0000

    Trash can x3, game_state is clearer

--- codenameGOD/main/game_state.lua
+++ codenameGOD/main/game_state.lua
@@ -1,4 +1,12 @@
 return {
+	-- Check if the game has cleared the introductory cutscene
+	intro_done = false,
+	-- Check if the game is at "playable" phase - 'true' means randall can move around/interact, and 'false' means...well he can't
+	in_gameplay = false,
+	-- Check if ticket is in the inventory
+	has_ticket = false,
+	-- Check if Randall is wearing his shoes
+	both_shoes = false, 
 	first_item_picked = false,
 	drawer_opened = false,
 	ticket_moved = false
