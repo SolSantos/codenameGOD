d9c2017d810a911eef3d107311f5ccb5d493819e
Guilherme Silva <32681069+gcsilva11@users.noreply.github.com>
2021-03-19 22:32:50 +0000

    Cleared up a bit the game state naming, tuned up the trash can hitting, inventory closes on transictioning places
    Im tired and it's late

--- codenameGOD/main/game_state.lua
+++ codenameGOD/main/game_state.lua
@@ -3,10 +3,10 @@ return {
 	intro_done = false,
 	-- Check if the game is at "playable" phase - 'true' means randall can move around/interact, and 'false' means...well he can't
 	in_gameplay = false,
-	-- Check if ticket is in the inventory
-	has_ticket = false,
 	-- Check if Randall is wearing his shoes
-	both_shoes = false, 
+	both_shoes = false,
+	-- Check if first puzzle is completed
+	allowed_leave = false,
 	first_item_picked = false,
 	drawer_opened = false,
 	ticket_moved = false
