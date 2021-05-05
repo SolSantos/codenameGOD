77444b47d367807f1ccd7f837b2b26b6f930302c
drmargarido <drmargarido@gmail.com>
2021-05-05 15:01:33 +0100

    Implemented shader to make the ouija board letter smoothly get focused when hovered.

--- codenameGOD/main/game/room/ouija_mode.lua
+++ codenameGOD/main/game/room/ouija_mode.lua
@@ -1,5 +1,6 @@
 local inside_gameobject = require("main.inside_gameobject")
 local game_state = require("main.game_state")
+local utils = require("main.utils")
 local SHAKE = 1
 local SHAKE_MOVING = 3
 local SHAKE_MOVING_SLEEP = 0.02
@@ -24,30 +25,30 @@ local ouija_mapping =  {
 	J =     {x=496, y=272, width=30,  height=50},
 	K =     {x=539, y=289, width=30,  height=50},
 	L =     {x=583, y=272, width=30,  height=50},
-	M =     {x=627, y=289, width=30,  height=50},
+	M =     {x=627, y=289, width=32,  height=50},
 	N =     {x=86,  y=195, width=30,  height=50},
 	O =     {x=135, y=177, width=30,  height=50},
 	P =     {x=183, y=195, width=30,  height=50},
-	Q =     {x=227, y=177, width=30,  height=50},
-	R =     {x=272, y=195, width=30,  height=50},
+	Q =     {x=227, y=173, width=30,  height=54},
+	R =     {x=272, y=193, width=30,  height=52},
 	S =     {x=318, y=177, width=30,  height=50},
-	T =     {x=358, y=195, width=30,  height=50},
+	T =     {x=358, y=193, width=30,  height=52},
 	U =     {x=399, y=177, width=30,  height=50},
-	V =     {x=443, y=195, width=30,  height=50},
-	W =     {x=491, y=177, width=30,  height=50},
-	X =     {x=538, y=195, width=30,  height=50},
-	Y =     {x=582, y=177, width=30,  height=50},
-	Z =     {x=628, y=195, width=30,  height=50},
-	["1"] = {x=171, y=104, width=17,  height=33},
-	["2"] = {x=203, y=114, width=25,  height=33},
-	["3"] = {x=244, y=104, width=25,  height=33},
-	["4"] = {x=283, y=114, width=25,  height=33},
-	["5"] = {x=327, y=104, width=25,  height=33},
-	["6"] = {x=368, y=114, width=25,  height=33},
-	["7"] = {x=408, y=104, width=25,  height=33},
-	["8"] = {x=447, y=114, width=25,  height=33},
-	["9"] = {x=488, y=104, width=25,  height=33},
-	["0"] = {x=532, y=114, width=25,  height=33},
+	V =     {x=443, y=190, width=37,  height=55},
+	W =     {x=491, y=177, width=32,  height=50},
+	X =     {x=538, y=193, width=30,  height=52},
+	Y =     {x=582, y=177, width=33,  height=50},
+	Z =     {x=624, y=193, width=34,  height=52},
+	["1"] = {x=168, y=101, width=23,  height=39},
+	["2"] = {x=201, y=109, width=29,  height=39},
+	["3"] = {x=241, y=101, width=29,  height=39},
+	["4"] = {x=280, y=105, width=29,  height=39},
+	["5"] = {x=324, y=101, width=29,  height=39},
+	["6"] = {x=365, y=109, width=29,  height=39},
+	["7"] = {x=405, y=101, width=29,  height=39},
+	["8"] = {x=444, y=109, width=29,  height=39},
+	["9"] = {x=485, y=101, width=29,  height=39},
+	["0"] = {x=529, y=109, width=29,  height=39},
 	YES =   {x=235, y=383, width=90,  height=38},
 	NO =    {x=423, y=383, width=57,  height=38},
 	DONE =  {x=299, y=30,  width=121, height=40}
@@ -81,13 +82,21 @@ end
 -- letter here may be nil, [A-Z0-9], or the words YES, NO and DONE
 local handle_selected_letter = function(room, dt, letter)
 	if not letter then
+		room.leave_letter = true
+		room.hover_elapsed = utils.clamp(room.hover_elapsed - (dt * 4), 0, LETTER_WAIT_TIME)
 		return
 	end
 	
-	room.hover_elapsed = room.hover_elapsed + dt
-	if not room.moved_after_selection or room.hover_elapsed < LETTER_WAIT_TIME then
+	room.hover_elapsed = utils.clamp(room.hover_elapsed + dt, 0, LETTER_WAIT_TIME)
+	if letter ~= room.letter then
+		room.hover_elapsed = 0
+	end
+	room.letter = letter
+
+	if not room.moved_after_selection or not room.leave_letter or room.hover_elapsed < LETTER_WAIT_TIME then
 		return
 	end
+	room.leave_letter = false
 	
 	if letter == "YES" then
 		if room.ouija_name_done then
@@ -112,7 +121,17 @@ local handle_selected_letter = function(room, dt, letter)
 	end
 
 	room.moved_after_selection = false
-	room.hover_elapsed = 0
+end
+
+local get_letter_scaled_rect = function(letter, ouija_offset)
+	local w, h = window.get_size()
+	local scale = vmath.vector3(w / WIDTH, h / HEIGHT, 1.0)
+	local rect = ouija_mapping[letter]
+	local rx = (rect.x + ouija_offset.x) * scale.x
+	local ry = (rect.y + ouija_offset.y) * scale.y
+	local rw = rect.width * scale.x
+	local rh = rect.height * scale.y
+	return vmath.vector4(rx, ry, rw, rh)
 end
 
 return {
@@ -131,29 +150,43 @@ return {
 		room.ouija_name_done = false
 		room.scared_elapsed = 0
 		room.talking_elapsed = 0
+		room.letter = nil
+		room.leave_letter = true
 	end,
 	update = function(room, dt)
 		local shake = SHAKE
 		local shake_sleep = SHAKE_SLEEP
+		local magnifier_pos = room.arms_position + MAGNIFIER_OFFSET
+
+		local w, h = window.get_size() 
+		local scale = vmath.vector3(w / WIDTH, h / HEIGHT, 1.0)
+		go.set(room.big_ouija_hitbox_url, "magnifier_radius", vmath.vector4(45 * scale.y, 0, 0, 0))
+
+		local letter = get_hovered_letter(room, magnifier_pos.x, magnifier_pos.y)
+		local ouija_offset = go.get_position(room.big_ouija_url) - (OUIJA_SIZE / 2)
+		if letter then
+			local pos = get_letter_scaled_rect(letter, ouija_offset)
+			go.set(room.big_ouija_hitbox_url, "letter_rect", pos)
+		end
+
 		if room.moved_hands then
 			go.set_position(room.arms_position, room.randall_arms_url)
+			go.set(room.big_ouija_hitbox_url, "magnifier_pos", vmath.vector4(magnifier_pos.x * scale.x, magnifier_pos.y * scale.y, magnifier_pos.z, 0))
+			
 			room.moved_hands = false
-			room.hover_elapsed = 0
 			room.moved_after_selection = true
 			shake = SHAKE_MOVING
 			shake_sleep = SHAKE_MOVING_SLEEP
 			msg.post("/randall", "get_scared")
 			room.scared_elapsed = 0
 		else
-			local magnifier_pos = room.arms_position + MAGNIFIER_OFFSET
-			local letter = get_hovered_letter(room, magnifier_pos.x, magnifier_pos.y)
-			handle_selected_letter(room, dt, letter)
-
 			room.scared_elapsed = room.scared_elapsed + dt
 			if room.scared_elapsed > SCARED_DEACTIVATION_TIME then
 				msg.post("/randall", "idle")
 			end
 		end
+		handle_selected_letter(room, dt, letter)
+		go.set(room.big_ouija_hitbox_url, "hover_elapsed", vmath.vector4(room.hover_elapsed, 0, 0, 0))
 
 		-- Since we keep the balloon in display for a big time we want to manually 
 		-- tell randall to stop talking earlier.
--- codenameGOD/render/over.fp
+++ codenameGOD/render/over.fp
@@ -4,9 +4,14 @@ varying mediump vec2 var_texcoord0;
 uniform lowp sampler2D texture_sampler;
 uniform lowp vec4 tint;
 
+uniform lowp vec4 magnifier_radius;
+uniform lowp vec4 magnifier_pos;
+uniform lowp vec4 letter_rect;
+uniform lowp vec4 hover_elapsed;
+
 // Color comparison with some error allowed
 bool is_similar_color(vec4 c1, vec4 c2){
-	float threshold = 0.02;
+	float threshold = 0.001;
 	return c1.r >= c2.r - threshold && c1.r <= c2.r + threshold && 
 		   c1.g >= c2.g - threshold && c1.g <= c2.g + threshold &&
 		   c1.b >= c2.b - threshold && c1.b <= c2.b + threshold &&
@@ -35,19 +40,35 @@ bool other_color_in_neighborhood(lowp vec4 color, vec2 pos, vec2 size){
 	return false;
 }
 
-void main()
-{
-	const vec4 letter_color = vec4(0.0, 0.078, 0.125, 1.0);
+bool is_inside_circle(lowp vec4 center, float radius, lowp vec2 other_pos){
+	if(radius == 0.0) return false;
+	float dx = abs(other_pos.x - center.x);
+	float dy = abs(other_pos.y - center.y);
+	return dx * dx + dy * dy <= radius * radius;
+}
 
+bool is_inside_rect(vec4 rect, vec2 pos){
+	return pos.x >= rect.x && pos.x <= rect.x + rect.z && pos.y >= rect.y && pos.y <= rect.y + rect.w;
+}
+
+void main(){
+	const lowp float LETTER_WAIT_TIME = 1.6;
+	const vec4 letter_color = vec4(0.0, 0.078, 0.125, 1.0);
+	
 	lowp vec2 size = gl_FragCoord.xy / var_texcoord0.xy;
 	lowp vec4 tint_pm = vec4(tint.xyz * tint.w, tint.w);
 	lowp vec4 color = texture2D(texture_sampler, gl_FragCoord.xy / size) * tint_pm;
-
-	if(is_similar_color(color, letter_color)){
-		if(other_color_in_neighborhood(letter_color, gl_FragCoord.xy, size)) {
-			color = vec4(1, 1, 1, 1);
+	
+	if(is_inside_circle(magnifier_pos, magnifier_radius.r, gl_FragCoord.xy)){
+		if(is_inside_rect(letter_rect, gl_FragCoord.xy)){
+			if(is_similar_color(color, letter_color) && other_color_in_neighborhood(letter_color, gl_FragCoord.xy, size)) {
+				float progress = clamp(hover_elapsed.x / LETTER_WAIT_TIME, 0.0, 1.0);
+				float red = progress;
+				float green = letter_color.g + (1.0 - letter_color.g) * progress;
+				float blue = letter_color.b + (1.0 - letter_color.b) * progress;
+				color = vec4(red, green, blue, 1.0);
+			}
 		}
 	}
-	
 	gl_FragColor = color;
 }
