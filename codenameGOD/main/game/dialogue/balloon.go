ddbb58dad5e928fbe74fae70b31728f528464bbd
drmargarido <drmargarido@gmail.com>
2021-05-27 19:56:48 +0100

    Added needed code to manage the middle size balloon.

--- codenameGOD/main/game/dialogue/balloon.go
+++ codenameGOD/main/game/dialogue/balloon.go
@@ -326,7 +326,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/balloon.atlas\"\n"
-  "default_animation: \"balloon3\"\n"
+  "default_animation: \"balloon2\"\n"
   "material: \"/render/over.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -501,6 +501,154 @@ embedded_components {
   position {
     x: 210.0
     y: 23.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "middle_label"
+  type: "label"
+  data: "size {\n"
+  "  x: 310.0\n"
+  "  y: 90.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "scale {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_NW\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: true\n"
+  "text: \"Default\"\n"
+  "font: \"/builtins/fonts/system_font.font\"\n"
+  "material: \"/render/over_label.material\"\n"
+  ""
+  position {
+    x: -222.0
+    y: 78.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "middle_skip"
+  type: "sprite"
+  data: "tile_set: \"/assets/items.atlas\"\n"
+  "default_animation: \"skip_dialogue_small_dark\"\n"
+  "material: \"/render/over.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 96.0
+    y: -2.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "middle_label_inverted"
+  type: "label"
+  data: "size {\n"
+  "  x: 310.0\n"
+  "  y: 90.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "scale {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_NW\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: true\n"
+  "text: \"Default\"\n"
+  "font: \"/builtins/fonts/system_font.font\"\n"
+  "material: \"/render/over_label.material\"\n"
+  ""
+  position {
+    x: -104.0
+    y: 78.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "middle_skip_inverted"
+  type: "sprite"
+  data: "tile_set: \"/assets/items.atlas\"\n"
+  "default_animation: \"skip_dialogue_small_dark\"\n"
+  "material: \"/render/over.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 213.0
+    y: -2.0
     z: 0.1
   }
   rotation {
